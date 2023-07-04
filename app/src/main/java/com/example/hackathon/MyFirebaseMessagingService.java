71eea08d31ec05201be65521558b50d118197870
parkUiery <parkuiery@gmail.com>
2023-07-04 16:27:07 +0900

    디바이스 토큰

--- app/src/main/java/com/example/hackathon/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/hackathon/MyFirebaseMessagingService.java
@@ -1,2 +1,30 @@
-package com.example.hackathon;public class MyFirebaseMessagingService {
+package com.example.hackathon;
+
+import android.content.Intent;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    @Override
+    public void onNewToken(@NonNull String token) {
+        super.onNewToken(token);
+
+        Log.d("TEST",token);
+        Intent intent = new Intent(MyFirebaseMessagingService.this, LoginActivity.class);
+        intent.putExtra("토큰",token);
+    }
+    @Override
+    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+        //수신한 메시지를 처리
+    }
 }
