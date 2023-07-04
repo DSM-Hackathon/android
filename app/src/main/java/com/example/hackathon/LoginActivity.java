87389b7c1413fcf6f781222d2910c7f84f961f89
parkUiery <parkuiery@gmail.com>
2023-07-04 16:26:39 +0900

    로그인

--- app/src/main/java/com/example/hackathon/LoginActivity.java
+++ app/src/main/java/com/example/hackathon/LoginActivity.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.widget.Toast;
 
 import com.example.hackathon.databinding.ActivityLoginBinding;
+import com.google.firebase.iid.FirebaseInstanceId;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -25,21 +26,29 @@ public class LoginActivity extends AppCompatActivity {
         binding = ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        binding.loginLgoinBtn.setOnClickListener(new View.OnClickListener() {
+        binding.loginStartBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 logIn();
+            }
+        });
+
+        binding.loginGoSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+                startActivity(intent);
             }
         });
     }
 
     private void logIn() {
         String accountId = binding.loginIdEt.getText().toString();
-        String email = binding.loginPwEt.getText().toString();
+        String password = binding.loginPasswordEt.getText().toString();
 
         if(accountId.length() == 0) {
             Toast.makeText(LoginActivity.this, "아이디를 입력해주세요",Toast.LENGTH_SHORT).show();
-        } else if (email.length() == 0) {
+        } else if (password.length() == 0) {
             Toast.makeText(LoginActivity.this , "비밀번호를 입력해주세요", Toast.LENGTH_SHORT).show();
         }else {
             logInResponse();
@@ -48,15 +57,18 @@ public class LoginActivity extends AppCompatActivity {
 
     private void logInResponse() {
         String accountId = binding.loginIdEt.getText().toString();
-        String email = binding.loginPwEt.getText().toString();
+        String password = binding.loginPasswordEt.getText().toString();
 
-        LogInRequest logInRequest = new LogInRequest(accountId, email);
-        SeverApi severApi = ApiProvider.getInstnace().create(SeverApi.class);
+        String token;
+
+        token = FirebaseInstanceId.getInstance().getToken();
+
+        LogInRequest logInRequest = new LogInRequest(accountId, password, token);
+        SeverApi severApi = ApiProvider.getInstance().create(SeverApi.class);
 
         severApi.Login(logInRequest).enqueue(new Callback<LogInResponse>() {
             @Override
             public void onResponse(Call<LogInResponse> call, Response<LogInResponse> response) {
-                Log.d("TEST", response.toString());
                 if(response.isSuccessful()){
                     Toast.makeText(LoginActivity.this, "로그인에 성공 했습니다!",Toast.LENGTH_SHORT).show();
                     Intent intent = new Intent(getApplicationContext(), MainActivity.class);
